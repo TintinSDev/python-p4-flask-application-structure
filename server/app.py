b1ff589ed9e4ae5f652e461825f6d8858284fe23
Martin <mmaina290@gmail.com>
2024-02-27 21:35:52 +0300

    done

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return '<h2> Hello World! </h2>'
+
+@app.route('/<string:username>')
+def index(username):
+    return f'<h2> Welcome to flask {username}! </h2>'
+
+if __name__ == '__main__':
+    app.run(port=5555)
